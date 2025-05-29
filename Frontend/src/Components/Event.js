dba885552fcee76b313e8b4e5f646039174f4663
James <jimmykimmy380@gmail.com>
2025-05-29 16:25:31 +1000

    updated

--- Frontend/src/Components/Event.js
+++ Frontend/src/Components/Event.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "../Pages/Events.css";
 
 function Event({event}) {
   return (
     <div className="event">
       <a href={event.url}>
-      <img src={event.imageUrl} alt={event.name} style={{width:100, height:80}}/>
+      <img src={event.imageUrl} alt={event.name} className={"img"}/>
         <div>
           <h3>{event.name}</h3>
-          <p>{event.date}</p>
-          <p>{`${event.timeStart}  -  ${event.timeEnd}`}</p>
+          <div className="DateTime">
+            <p>{event.date}</p>
+            <p>{`${event.timeStart}  -  ${event.timeEnd}`}</p>
+          </div>
         </div>
       </a>
     </div>
--- Frontend/src/Pages/Events.js
+++ Frontend/src/Pages/Events.js
@@ -4,20 +4,12 @@ import "./Events.css";
 
 const eventList = [
   {
-    name: "React Meetup",
-    date: "2025-06-01",
-    timeStart: "10:00 AM",
-    timeEnd: "12:00 PM",
-    url: "https://example.com/react-meetup",
-    imageUrl: "https://example.com/react-meetup.jpg",
-  },
-  {
-    name: "Vue Workshop",
+    name: "Mueseum",
     date: "2025-06-05",
     timeStart: "2:00 PM",
     timeEnd: "4:00 PM",
-    url: "https://example.com/vue-workshop",
-    imageUrl: "https://example.com/vue-workshop.jpg",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
   },
   {
     name: "Mueseum",
@@ -27,14 +19,134 @@ const eventList = [
     url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
     imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
   },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "WIN Stadium Woman's Soccer",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://gothunderbirds.ca/images/2023/11/9/RK9_2535.jpg?width=1080&height=608&mode=crop&format=jpg&quality=80",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Mueseum",
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_3000,h_2000/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/kxckqbyccilrbh1g2ezg/NaturalHistoryMuseumTicketinLosAngeles-KlookAustralia.jpg",
+  },
+  {
+    name: "Aquarium of the Bizarre", 
+    date: "2025-06-05",
+    timeStart: "2:00 PM",
+    timeEnd: "4:00 PM",
+    url: "https://www.klook.com/en-AU/activity/129401-natural-history-museum-ticket-in-los-angeles/",
+    imageUrl: "https://www.visitsealife.com/sydney/media/f4webh41/hero-image.png",
+  },
 ];
 
 
 
-function Events() {
+function Events() {//30 Character Limit for title
   return (
     <div>
-      <h2>Upcoming Events</h2>
+      <h2 className="Subtitle">Upcoming Events</h2>
       <div className="eventList">
         {eventList.map((eventItem, index) => (
           <Event key={index} event={eventItem} />
