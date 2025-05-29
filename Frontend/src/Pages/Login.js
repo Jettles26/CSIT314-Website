75e510819e6e2b5581552320c67bca51ef4624fa
James <jimmykimmy380@gmail.com>
2025-05-29 16:04:25 +1000

    publish

---
+++ Frontend/src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css"; // optional: for styling
+
+function Login() {
+  return (
+
+      <div>
+        <h1>We are going to need a login feature, eh.</h1>
+      </div>
+  );
+}
+
+export default Login;
